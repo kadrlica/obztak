de07c7b651f8bf38e918803ad742beeac9f1d6c9
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-02-08 01:26:34 -0600

    psycopg2

--- maglites/utils/__init__.py
+++ maglites/utils/__init__.py
@@ -2,5 +2,9 @@
 Observation planning for the Magellanic Satellites Survey.
 """
 
-from .database import Database
+try:
+    from .database import Database
+except ImportError:
+    pass
+
 from .ortho import datestring
