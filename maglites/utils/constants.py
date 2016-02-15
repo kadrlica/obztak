1ed306634c471a578bfc1878106db22dc142fa77
Alex Drlica-Wagner <kadrlica@fnal.gov>
2016-02-15 15:01:26 -0300

    Added some figure constants (currently unused)

--- maglites/utils/constants.py
+++ maglites/utils/constants.py
@@ -3,14 +3,23 @@ Constants.
 """
 import ephem
 from collections import OrderedDict as odict
+import numpy as np
 
+# Plotting DECam
+DECAM=1.1 # DECam radius (deg)
+# Marker size depends on figsize and DPI
+FIGSIZE=(10.5,8.5)
+SCALE=np.sqrt((8.0*6.0)/(FIGSIZE[0]*FIGSIZE[1]))
+DPI=80;
+
+
+# LMC and SMC
 RA_LMC = 80.8939
 DEC_LMC = -69.7561
 RA_SMC = 13.1867
 DEC_SMC = -72.8286
 
 # http://www.ctio.noao.edu/noao/content/coordinates-observatories-cerro-tololo-and-cerro-pachon
-
 LON_CTIO = '-70:48:23.49'
 LAT_CTIO = '-30:10:10.78'
 ELEVATION_CTIO = 2206.8 # m
