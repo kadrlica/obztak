04aabe5f39098b2b5082acf91cf60a7c79dea2cf
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-01-03 17:42:22 -0600

    Adding tests of database fields

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -80,6 +80,21 @@ def test_sispi_fields():
 def test_database_fields():
     fields = obztak.field.FieldArray.load_database('db-fnal')
 
+    # No database connection
+    if len(fields) == 0: return
+
+    idx = [0,17,357,1703]
+    test = np.rec.array([
+        (5143,101.331075,-55.618165,'g',90.0,1,1,'2016/02/11 04:56:51.972',1.29,87.4149,97.1638, 42.6655),
+        (5345,106.011075,-58.41872, 'r',90.0,1,1,'2016/02/11 05:30:32.472',1.35,0.0015, 98.7751, 46.357625),
+        (6062,157.487654,-76.852859,'g',90.0,1,1,'2016/02/13 09:00:12.732',1.62,1.4089, 106.6357,49.368042),
+        (2206,345.810525,-70.197803,'g',90.0,2,1,'2016/06/30 08:37:52.759',1.31,1.6166, 91.3154, -8.633625)],
+        dtype=[('HEX','<i8'),('RA','<f8'),('DEC','<f8'),('FILTER','S1'),
+               ('EXPTIME','<f8'),('TILING','<i8'),('PRIORITY','<i8'),
+               ('DATE','S30'), ('AIRMASS','<f8'),('SLEW','<f8'),
+               ('MOONANGLE','<f8'),('HOURANGLE','<f8')])
+
+    np.testing.assert_equal(fields[idx],test)
 
 if __name__ == "__main__":
     import argparse
