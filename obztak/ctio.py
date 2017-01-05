a064cebcf86a27945f78dee90614e175dac3c765
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-01-05 00:46:00 -0600

    Adding timezone information to CTIO

--- obztak/ctio.py
+++ obztak/ctio.py
@@ -5,10 +5,12 @@ Generic python script.
 __author__ = "Alex Drlica-Wagner"
 
 import ephem
+import pytz
 
 from obztak.utils import constants
 
 # WARNING: copy.deepcopy doesn't work for ephem.Observer
+# need to use ephem.Observer.copy
 
 class CTIO(ephem.Observer):
     """ Utility class for defining CTIO """
@@ -16,7 +18,42 @@ class CTIO(ephem.Observer):
         self.lon = constants.LON_CTIO
         self.lat = constants.LAT_CTIO
         self.elevation = constants.ELEVATION_CTIO
+        self.tz = pytz.timezone('Chile/Continental')
+        self.twilight = '-14'
 
+    def utc2local(self, utc):
+        """ Convert ephem in UTC to local observatory time """
+        utc_dt = pytz.utc.localize(ephem.Date(utc).datetime())
+        return utc_dt.astimezone(self.tz)
+
+    def local2utc(self, local):
+        """ Convert string of local time to UTC """
+        local_dt = self.tz.localize(ephem.Date(local).datetime())
+        return utc_dt.astimezone(self.tz)
+
+    def next_sunrise(self, date=None):
+        obs = self.copy()
+        if date: obs.date = ephem.Date(date)
+        obs.horizon = self.twilight
+        return obs.next_rising(ephem.Sun(), use_center=True)
+
+    def next_sunset(self, date=None):
+        obs = self.copy()
+        if date: obs.date = ephem.Date(date)
+        obs.horizon = self.twilight
+        return obs.next_setting(ephem.Sun(), use_center=True)
+
+    def previous_sunrise(self, date=None):
+        obs = self.copy()
+        if date: obs.date = ephem.Date(date)
+        obs.horizon = self.twilight
+        return obs.next_rising(ephem.Sun(), use_center=True)
+
+    def previous_sunset(self, date=None):
+        obs = self.copy()
+        if date: obs.date = ephem.Date(date)
+        obs.horizon = self.twilight
+        return obs.next_setting(ephem.Sun(), use_center=True)
 
 if __name__ == "__main__":
     import argparse
