f8f6799731402b90853d4aeedfa9a52f773a3c29
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-01-13 10:38:33 -0600

    Moving observatory constraints

--- obztak/ctio.py
+++ obztak/ctio.py
@@ -3,11 +3,14 @@
 Generic python script.
 """
 __author__ = "Alex Drlica-Wagner"
-
+import os
 import ephem
 import pytz
 
+import numpy as np
+
 from obztak.utils import constants
+from obztak.utils import projector as proj
 
 # WARNING: copy.deepcopy doesn't work for ephem.Observer
 # need to use ephem.Observer.copy
@@ -21,6 +24,7 @@ class CTIO(ephem.Observer):
         self.elevation = constants.ELEVATION_CTIO
         self.tz = pytz.timezone('Chile/Continental')
         self.twilight = '-14'
+        self._load_constraints()
 
     def utc2local(self, utc):
         """ Convert ephem in UTC to local observatory time """
@@ -55,6 +59,33 @@ class CTIO(ephem.Observer):
         if date: obs.date = ephem.Date(date)
         obs.horizon = self.twilight
         return obs.next_setting(ephem.Sun(), use_center=True)
+
+    def _load_constraints(self, filename=None):
+        """ Load Blanco constraint data """
+        if filename is None:
+            from obztak.utils import fileio
+            filename = os.path.join(fileio.get_datadir(),'blanco_hour_angle_limits.dat')
+        self.constraints = np.recfromtxt(filename, names=True)
+
+        # ADW: This is not very pythonic....
+        ha_degrees = np.tile(0., len(self.constraints['HA']))
+        for ii in range(0, len(self.constraints['HA'])):
+            ha_degrees[ii] = proj.hms2dec(self.constraints['HA'][ii])
+
+        # Buffer to protect us from the chicken
+        ha_degrees -= 1.25 
+        
+        self.ha_degrees = ha_degrees
+        return self.ha_degrees
+
+    def hour_angle_limit(self, dec):
+        # Avoid relying on scipy.interpolate
+        return np.interp(dec,self.constraints['Dec'],self.ha_degrees,left=-1,right=-1)
+
+    def airmass_limit(self, dec):
+        # Avoid relying on scipy.interpolate
+        return np.interp(dec,self.constraints['Dec'], self.constraints['AirmassLimit'],left=-1,right=-1)
+
 
 if __name__ == "__main__":
     import argparse
