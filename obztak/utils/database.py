2dab82efa67678be0a49acee47c2968f1b175fab
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-01-02 11:20:47 -0600

    reset db info

--- obztak/utils/database.py
+++ obztak/utils/database.py
@@ -32,9 +32,7 @@ class Database(object):
             if hostname in ('observer2.ctio.noao.edu','observer3.ctio.noao.edu'):
                 return 'db-ctio'
             else:
-                # REMEMBER TO RESET AFTER POWER OUTAGE
-                return 'db-ctio'
-                #return 'db-fnal'
+                return 'db-fnal'
         return dbname
 
     def parse_config(self, filename=None, section='db-fnal'):
