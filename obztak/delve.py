005e6c5d3c2cafdb1132393a2fee3045a00cb943
Alex Drlica-Wagner <kadrlica@fnal.gov>
2020-11-22 13:29:34 -0300

    tweaks to wide

--- obztak/delve.py
+++ obztak/delve.py
@@ -682,7 +682,7 @@ class DelveScheduler(Scheduler):
 class DelveTactician(Tactician):
     CONDITIONS = odict([
         (None,       [1.0, 2.0]),
-        ('wide',     [1.0, 1.4]),
+        ('wide',     [1.0, 1.5]),
         ('deep',     [1.0, 1.4]),
         ('mc',       [1.0, 2.0]),
         ('gw',       [1.0, 2.0]),
@@ -723,15 +723,13 @@ class DelveTactician(Tactician):
             #sel &= (np.char.count('iz',self.fields['FILTER']) > 0)
             #weight += 1e2 * (np.char.count('i',self.fields['FILTER']) > 0)
         #elif (self.moon.phase >= 45) and (self.moon.alt > 0.175):
-        elif (self.moon.phase >= 40) and (self.moon.alt > 0.0):
+        elif (self.moon.phase >= 30) and (self.moon.alt > 0.0):
             # Moon is more than half full; do r,i
             sel &= (np.char.count('ri',self.fields['FILTER']) > 0)
-        elif (self.moon.phase >= 20) and (self.moon.alt > 0.0):
-            # Moon is more than half full; do r,i
-            sel &= (np.char.count('gri',self.fields['FILTER']) > 0)
         else:
-            # Moon is faint or down; do g,r (unless none available)
-            sel &= (np.char.count('gr',self.fields['FILTER']) > 0)
+            # Moon is faint or down; do g,r,i
+            sel &= (np.char.count('gri',self.fields['FILTER']) > 0)
+            # Alternatively, only red bands if others unavailable
             #weight += 1e8 * (np.char.count('iz',self.fields['FILTER']) > 0)
         return sel
 
