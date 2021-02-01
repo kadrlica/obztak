c7e7d1ccb25feb48565cb1241f1762dec96e065d
Alex Drlica-Wagner <kadrlica@fnal.gov>
2021-02-01 13:01:59 -0600

    Updating fields

--- obztak/scratch/update-fields.py
+++ obztak/scratch/update-fields.py
@@ -22,7 +22,7 @@ parser.add_argument('new')
 parser.add_argument('-o','--outfile',default='update_target_fields.csv')
 args = parser.parse_args()
 
-db = DelveFieldArray.load_database()
+db  = DelveFieldArray.load_database()
 old = DelveFieldArray.load(args.old)
 new = DelveFieldArray.load(args.new)
 
@@ -33,6 +33,7 @@ if len(old) != len(new):
 
 delve = np.in1d(new.unique_id,db.unique_id)
 
+### New fields (not done before, now done)
 #done = (new['PRIORITY'] < 0) & (old['PRIORITY'] >= 0)
 done = (new['PRIORITY'] < 0) & np.in1d(new.unique_id, old.unique_id[old['PRIORITY'] >= 0])
 
@@ -40,7 +41,17 @@ plt.figure()
 smap = skymap.SurveyMcBryde()
 smap.draw_fields(new[done & ~delve])
 smap.draw_des()
-plt.title('New')
+plt.title('Exposures newly considered done')
+plt.show()
+
+### New fields that were previously considered done and are not now
+undone = (new['PRIORITY'] >= 0) & np.in1d(new.unique_id, old.unique_id[old['PRIORITY']<0])
+
+plt.figure()
+smap = skymap.SurveyMcBryde()
+smap.draw_fields(new[undone & ~delve])
+smap.draw_des()
+plt.title('Exposures no longer considered done')
 plt.show()
 
 # Write here
@@ -50,13 +61,14 @@ out = DelveFieldArray.load(args.old)
 print("Running DelveSurvey.update_covered_fields...")
 update = obztak.delve.DelveSurvey.update_covered_fields(old)
 done = (update['PRIORITY'] < 0) & (old['PRIORITY'] >= 0)
-delve = np.in1d(update.unique_id,db.unique_id)
+obs = np.in1d(update.unique_id,db.unique_id)
 
 plt.figure()
 smap = skymap.SurveyMcBryde()
-smap.draw_fields(update[done & ~delve])
+smap.draw_fields(update[done & ~obs])
+smap.draw_des()
 #smap.draw_fields(update[done])
-plt.title('Update')
+plt.title('Exposures updated in output')
 
 print("Writing %s..."%args.outfile)
 update.write(args.outfile)
@@ -65,3 +77,14 @@ update.write(args.outfile)
 assert len(fileio.read_csv(args.old)) == len(fileio.read_csv(args.outfile))
 
 print("REMINDER: gzip the output file and move to data directory.")
+
+# Check with DEROSITAS
+#deros_g = DelveFieldArray.load('derositas/data/test_g_all_20210127.csv')
+#deros_i = DelveFieldArray.load('derositas/data/test_i_all_20210127.csv')
+#plt.figure()
+#smap = skymap.SurveyMcBryde()
+#smap.draw_fields(deros_g)
+#smap.draw_fields(deros_i)
+#smap.draw_des()
+##smap.draw_fields(update[done])
+#plt.title('DeROSTIAS TODO')
